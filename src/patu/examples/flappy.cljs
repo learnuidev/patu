d1fa9fe2fa43849e77a8d7f4d1f7c39c22460aec
Vishal Gautam <vishal.gautam@sofdesk.com>
2021-07-29 22:17:44 -0400

    updated doc

--- src/patu/examples/flappy.cljs
+++ src/patu/examples/flappy.cljs
@@ -1,26 +1,36 @@
 (ns patu.examples.flappy
   (:require [patu.core :as p]
             [patu.loaders :as l]
-            [patu.events :as evt]))
+            [patu.events :as evt]
+            [patu.utils :refer [jget  jset-in jget-in jset!]]))
 
-;; 0. Initialize
+;; 1. Initialize App
 (p/kaboom {:canvas (js/document.getElementById "app")
            :global true
            :fullscreen false})
 
-;; 1. Load Assets
+;; 2. Load Assets
 (l/load-root "https://kaboomjs.com/pub/examples/");
-(l/load-sprite  :bg, "img/bg.png");
-(l/load-sprite  :birdy, "img/birdy.png");
+(l/load-sprite [[:bg, "img/bg.png"];
+                [:birdy, "img/birdy.png"]
+                [:pipe, "img/pipe.png"]]);
+(l/load-sound [[:score "sounds/score.mp3"]
+               [:wooosh "sounds/wooosh.mp3"]
+               [:hit "sounds/hit.mp3"]])
 
+(comment
+  (jget (p/get-comp :player) :pos)
+  (jget-in (p/get-comp :player) [:pos :x]))
 
-;; Custom Event handler
+;; 3. Register Event handlers
 (evt/reg-event
  :player/go-south
  (fn [_ [_ id]]
-   (let [player (p/get-comp id)]
-     (set! (.. player -pos -y) (+ 10 (.. player -pos -y))))))
+   (let [player (p/get-comp id)
+         newy (+ 10 (jget-in player [:pos :y]))]
+     (jset-in player [:pos :y] newy))))
 
+;; 4. Register Components
 (evt/dispatch-n
  [[:comp/reg :player [[:sprite :birdy]
                       [:solid]
@@ -28,27 +38,16 @@
   [:comp/reg [[:sprite :birdy]
               [:solid]
               [:pos [250 100]]
-              :birdy]]
-  ;; Multiple component registration - ideal for spawning enemies
-  #_[:comp/reg-n
-     (for [n (range 10)]
-       [[:sprite :birdy]
-        [:pos [(+ 30 (* 60 n)) (+ 30 (* 60 n))]]
-        [:solid]
-        :birdy])]])
+              :birdy]]])
 
+;; 5. Use Event Handlers
 ;; Two ways to write handler function
 (evt/dispatch-n
  [[:action :player (fn [])]
                      ; (js/console.log "HELLO"))]
-   ;; Multiple Event Registation
+   ;; Multiple ways to register an Event
   [:key-down
-   ;; For simple cases use data syntax
-   [:right [[:player {:x 10}]]]
+   [:right [[:player {:x 10}]]]                         ;;  For simple cases use data syntax
    [:left  [[:player {:x -10}]]]
    [:up    {:player {:y -10}}]
-   ;; For more complex scenario: use event registration
-   [:down  {:dispatch [:player/go-south :player]}]
-   #_[:down  {:player {:y 10}}]]])
-
-(defn app [])
+   [:down  {:dispatch [:player/go-south :player]}]]])   ;;  For more complex scenario: use event registration (event handler needs to be registered first)
