8bf8423343038fd768d17aef33f3b31f5657de3b
learnuidev <learnuidev@gmail.com>
2022-02-27 12:38:44 -0500

    converted to deps.edn

---
+++ demo/src/learnui/patu/demo/core.cljs
@@ -0,0 +1,20 @@
+(ns learnui.patu.demo.core
+  (:require
+   #_[patu.examples.level.core :refer [app]]
+   [patu.examples.mario.core :refer [app]]
+   #_[patu.examples.rpg.core :refer [app]]
+   #_[patu.examples.flappy :refer [app]]
+   #_[patu.examples.drive :refer [app]]
+   #_[patu.examples.metro.core :refer [app]]
+   #_[patu.examples.patuv06.flappy :refer [app]]
+   #_[patu.examples.patuv06.contra :refer [app]]
+   #_[patu.examples.patuv06.aladdin :refer [app]]
+   #_[patu.examples.aladdin :refer [app]]
+   #_[patu.examples.patuv06.text :refer [app]]
+   #_[patu.examples.parallax :refer [app]]))
+
+(defn ^:dev/after-load start []
+  (app))
+
+(defn ^:export init []
+  (start))
